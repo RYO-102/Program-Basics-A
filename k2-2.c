a32009decdf34853714b568294a85cb6976caee5
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-04-17 18:39:28 +0900

    upload k2-2.c

---
+++ k2-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	float num_float;
+	double num_double;
+	int num_int;
+
+	num_float = 2442102;
+	num_double = 2442102;
+	num_int = 2442102;
+
+	printf("intŒ^•Ï”‚Ì’l‚Í%d‚Æ‚È‚è‚Ü‚·\n",num_int);
+	printf("ˆê•û,floatŒ^•Ï”‚Ì’l‚Í%f,doubleŒ^•Ï”‚Ì’l‚Í%f‚Æ‚È‚è‚Ü‚·\n",num_float, num_double);
+}
