71ad977706e80a2dc703176d9c524775eea4f9a6
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-04-17 18:37:16 +0900

    upload e3.c

---
+++ e3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y, sum;
+
+	printf("1‚Â–Ú‚Ì®”:");
+	scanf("%d",&x);
+	printf("2‚Â–Ú‚Ì®”:");
+	scanf("%d",&y);
+	sum = x + y;
+	printf("˜a‚Í%d‚Å‚·",sum);
+}
