8131515857cb37a22e2c8c795ad55f6abc132964
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-04-17 18:32:48 +0900

    upload e2.c

---
+++ e2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int year, month, day;
+	year = 2005;
+	month = 10;
+	day = 26;
+	printf("year = %d\n",year);
+	printf("month = %d\n",month);
+	printf("day = %d\n",day);
+	printf("¶”NŒ“ú : %d/%d/%d\n",year, month, day);
+}
