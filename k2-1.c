198a0eda5c37ec5a6010159a392c7e20915aa172
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-04-17 18:38:57 +0900

    upload k2-1.c

---
+++ k2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	float pi_float;
+	double pi_double;
+	int pi_int;
+
+	pi_float = 3.14159265359;
+	pi_double = 3.14159265359;
+	pi_int = 3.14159265359;
+
+	printf("‰~Žü—¦\n");
+	printf("pi_float = %f\n",pi_float);
+	printf("pi_double = %f\n",pi_double);
+	printf("pi_int = %d\n",pi_int);
+}
