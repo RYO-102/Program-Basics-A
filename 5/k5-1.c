c38ba8cc467d99f3512f4e5d2b8a4f0bdeeb3ffc
RYO-102 <ryou_ryou@docomo.ne.jp>
2024-06-10 17:36:29 +0900

    refactoring k5-1.c

--- 5/k5-1.c
+++ 5/k5-1.c
@@ -9,40 +9,24 @@ int main()
 
 	switch(month){
 		case 1:
-			printf("“~‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 2:
+		case 12:
 			printf("“~‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
 			break;
 		case 3:
-			printf("t‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 4:
-			printf("t‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 5:
 			printf("t‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
 			break;
 		case 6:
-			printf("‰Ä‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 7:
-			printf("‰Ä‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 8:
 			printf("‰Ä‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
 			break;
 		case 9:
-			printf("H‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 10:
-			printf("H‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
 		case 11:
 			printf("H‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
-			break;
-		case 12:
-			printf("“~‚É¶‚Ü‚ê‚Ü‚µ‚½\n");
 			break;
 		default:
 			printf("”ÍˆÍŠO‚Ì’l‚ª“ü—Í‚³‚ê‚Ä‚¢‚Ü‚·\n");
